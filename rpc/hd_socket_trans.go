6cabc4c42a6556b639282a6d4d4facb7d84a17b2
Garrett D'Amore <garrett@damore.org>
2018-10-01 15:09:56 -0700

    Remove connection pooling.
    The connection pooling logic in this code adds a certain amount
    of fragility, as the connections can wind up being reused even
    after the remote service has restarted.
    
    Ultimately, this service needs to be rethought so that it detects
    a remote connection close, and discards connections that have
    aborted.  This would require adding a pending read against the
    connection, in order to get notification that the remote socket
    has completed.
    
    Unfortunately, the current design is too simplistic to easily add
    that.  Removing the connection pooling may at least alleviate some
    pain, as a restart will only affect transactions that are in flight,
    at that time, but if the connection start up time is high (e.g. setting
    up a TLS protected TCP channel), then performance will drop.
    
    On the plus side, the removal of the pool means that clients will
    potentially achieve much greater levels of parallelization, as
    the pool size no longer limits this.  This is useful when running
    many requests that may be parallel but take a long time (such as
    requests that poll for physical disks.)

--- rpc/hd_socket_trans.go
+++ rpc/hd_socket_trans.go
@@ -21,33 +21,19 @@ package rpc
 
 import (
 	"net"
-	"runtime"
-	"sync"
-	"sync/atomic"
 	"time"
 )
 
-type halfDuplexConnEntry struct {
-	conn  net.Conn
-	timer *time.Timer
-}
-
 type halfDuplexSocketTransport struct {
 	idleTimeout time.Duration
-	connPool    chan *halfDuplexConnEntry
-	connCount   int32
-	nextid      uint32
+	poolSize    int
 	createConn  func() (net.Conn, error)
-	cond        sync.Cond
 }
 
 func newHalfDuplexSocketTransport() (hd *halfDuplexSocketTransport) {
 	hd = new(halfDuplexSocketTransport)
 	hd.idleTimeout = 0
-	hd.connPool = make(chan *halfDuplexConnEntry, runtime.NumCPU())
-	hd.connCount = 0
-	hd.nextid = 0
-	hd.cond.L = &sync.Mutex{}
+	hd.poolSize = 0
 	return
 }
 
@@ -67,97 +53,25 @@ func (hd *halfDuplexSocketTransport) SetIdleTimeout(timeout time.Duration) {
 
 // MaxPoolSize returns the max conn pool size of hprose socket client
 func (hd *halfDuplexSocketTransport) MaxPoolSize() int {
-	return cap(hd.connPool)
+	return hd.poolSize
 }
 
 // SetMaxPoolSize sets the max conn pool size of hprose socket client
 func (hd *halfDuplexSocketTransport) SetMaxPoolSize(size int) {
-	if size > 0 {
-		pool := make(chan *halfDuplexConnEntry, size)
-		for i := 0; i < len(hd.connPool); i++ {
-			select {
-			case pool <- <-hd.connPool:
-			default:
-			}
-		}
-		hd.connPool = pool
-	}
-}
-
-func (hd *halfDuplexSocketTransport) getConn() *halfDuplexConnEntry {
-	for hd.connPool != nil {
-		select {
-		case entry, ok := <-hd.connPool:
-			if !ok {
-				panic(ErrClientIsAlreadyClosed)
-			}
-			if entry.timer != nil {
-				entry.timer.Stop()
-			}
-			if entry.conn != nil {
-				return entry
-			}
-			continue
-		default:
-			return nil
-		}
-	}
-	panic(ErrClientIsAlreadyClosed)
-}
-
-func (hd *halfDuplexSocketTransport) fetchConn() (*halfDuplexConnEntry, error) {
-	hd.cond.L.Lock()
-	for {
-		entry := hd.getConn()
-		if entry != nil && entry.conn != nil {
-			hd.cond.L.Unlock()
-			return entry, nil
-		}
-		if int(atomic.AddInt32(&hd.connCount, 1)) <= cap(hd.connPool) {
-			hd.cond.L.Unlock()
-			conn, err := hd.createConn()
-			if err == nil {
-				return &halfDuplexConnEntry{conn: conn}, nil
-			}
-			atomic.AddInt32(&hd.connCount, -1)
-			return nil, err
-		}
-		atomic.AddInt32(&hd.connCount, -1)
-		hd.cond.Wait()
-	}
+	hd.poolSize = size
 }
 
 func (hd *halfDuplexSocketTransport) close() {
-	if hd.connPool != nil {
-		connPool := hd.connPool
-		hd.connPool = nil
-		hd.connCount = 0
-		close(connPool)
-		for entry := range connPool {
-			if entry.timer != nil {
-				entry.timer.Stop()
-			}
-			if entry.conn != nil {
-				entry.conn.Close()
-			}
-		}
-	}
 }
 
 func (hd *halfDuplexSocketTransport) closeConn(conn net.Conn) {
 	conn.Close()
-	atomic.AddInt32(&hd.connCount, -1)
 }
 
 func (hd *halfDuplexSocketTransport) sendAndReceive(
 	data []byte, context *ClientContext) ([]byte, error) {
-	entry, err := hd.fetchConn()
-	if err != nil {
-		hd.cond.Signal()
-		return nil, err
-	}
-	conn := entry.conn
-	err = conn.SetDeadline(time.Now().Add(context.Timeout))
+	conn, err := hd.createConn()
+
 	if err == nil {
 		err = hdSendData(conn, data)
 	}
@@ -167,23 +81,9 @@ func (hd *halfDuplexSocketTransport) sendAndReceive(
 	if err == nil {
 		err = conn.SetDeadline(time.Time{})
 	}
+	hd.closeConn(conn)
 	if err != nil {
-		hd.closeConn(conn)
-		hd.cond.Signal()
 		return nil, err
 	}
-	if hd.idleTimeout > 0 {
-		if entry.timer == nil {
-			entry.timer = time.AfterFunc(hd.idleTimeout, func() {
-				hd.closeConn(conn)
-				entry.conn = nil
-				entry.timer = nil
-			})
-		} else {
-			entry.timer.Reset(hd.idleTimeout)
-		}
-	}
-	hd.connPool <- entry
-	hd.cond.Signal()
 	return data, nil
 }
